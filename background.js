f51ad2838c20e51958782532eb6fd0e94db2bcba
brousermanager <bro.user.info@gmail.com>
2025-01-07 18:59:54 +0100

    gggggg

--- background.js
+++ background.js
@@ -1,15 +1,13 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "playClicked") {
-      chrome.tabs.query({url: "https://web.whatsapp.com/*"}, (tabs) => {
-        if (tabs.length > 0) {
-          chrome.tabs.sendMessage(tabs[0].id, {action: "sendMessage"});
-        } else {
-          chrome.tabs.create({url: "https://web.whatsapp.com/"}, (tab) => {
-            setTimeout(() => {
-              chrome.tabs.sendMessage(tab.id, {action: "sendMessage"});
-            }, 5000);
-          });
-        }
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.sidePanel
+      .setOptions({
+        path: 'popup.html',
+        enabled: true
+      })
+      .then(() => {
+        chrome.sidePanel.setPanelBehavior({
+          openPanelOnActionClick: true
+        });
       });
-    }
   });
+  
